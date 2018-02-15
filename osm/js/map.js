631efc52d88d8f6a9b52db7e064ce45443f3e691
likeaword <ben@satorilab.org>
2018-02-15 19:10:59 +0000

    Create map.js

---
+++ osm/js/map.js
@@ -0,0 +1,25 @@
+function addr_search() {
+    var inp = document.getElementById("addr");
+
+    $.getJSON('http://nominatim.openstreetmap.org/search?format=json&limit=5&q=' + inp.value, function(data) {
+        var items = [];
+
+        $.each(data, function(key, val) {
+            lat = val.lat;
+            lon = val.lon;
+            items.push("<li><a target=”_blank” href=https://openstreetmap.cymru/?h=" + lat + "&ll=" + lon + "&ch=12>" + val.display_name + " ... " + val.osm_type + "</a></li>");
+
+        });
+
+		$('#results').empty();
+        if (items.length != 0) {
+            $('<p>', { html: "Search results:" }).appendTo('#results');
+            $('<ul/>', {
+                'class': 'my-new-list',
+                html: items.join('')
+            }).appendTo('#results');
+        } else {
+            $('<p>', { html: "No results found" }).appendTo('#results');
+        }
+    });
+}
