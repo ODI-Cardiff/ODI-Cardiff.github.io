ccb4430b4d0d66de4a945521951bff40327dc596
likeaword <ben@satorilab.co.uk>
2018-02-17 18:20:13 +0000

    Zoom based on place type
    (fairly crude) process to extract the place type and set the zoom level accordingly

--- osm/js/map.js
+++ osm/js/map.js
@@ -1,16 +1,50 @@
 function addr_search() {
     var inp = document.getElementById("addr");
 
-    $.getJSON('https://nominatim.openstreetmap.org/search?format=json&limit=5&namedetails=1&q=' + inp.value, function(data) {
+    $.getJSON('https://nominatim.openstreetmap.org/search?format=json&limit=5&countrycodes=GB&namedetails=1&q=' + inp.value, function(data) {
         var items = [];
         var cymraeg;
+        var zoom;
+        var place_type;
 
         $.each(data, function(key, val) {
             lat = val.lat;
             lon = val.lon;
             cymraeg = val['namedetails']['name:cy'];
-  
-            items.push("<li><a target=”_blank” href=https://openstreetmap.cymru/?h=" + lat + "&ll=" + lon + "&ch=12>" + val.display_name + " ... " +  "..." + val.osm_type + cymraeg + "</a>  </li>");
+            place_type = val.type;
+          if (place_type === "country" || place_type === "continent" || place_type === "archipelago"){
+              zoom = 7;
+          } 
+            else if (place_type === "state" || place_type === "region" ){
+              zoom = 8;
+          } 
+            else if (place_type === "province" || place_type === "district" || place_type === "county" || place_type === "island" ){
+              zoom = 9;
+          }
+            else if (place_type === "municipality" || place_type === "islet"){
+              zoom = 12;
+          }
+            else if (place_type === "city"){
+              zoom = 13;
+          }
+            else if (place_type === "town" || place_type === "borough"){
+              zoom = 14;
+          }
+            else if (place_type === "suburb" || place_type === "village" || place_type === "hamlet"){
+              zoom = 15;
+          }
+            else if (place_type === "quarter" || place_type === "neighbourhood" || place_type === "city_block" || place_type === "allotments" || place_type === "square" || place_type === "locality"){
+              zoom = 16;
+          }
+            else if (place_type === "farm" || place_type === "plot" || place_type === "isolated_dwelling"){
+              zoom = 17;
+          }
+            else {
+              zoom =12;
+          }
+            
+
+            items.push("<li><a target=”_blank” href=https://openstreetmap.cymru/?h=" + lat + "&ll=" + lon + "&ch=" + zoom + ">" + cymraeg + "  ...  " + val.display_name + "  ...  " + val.osm_type + "</a>  </li>");
 
         });
 
